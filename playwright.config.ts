55afbb292732d9860debff712a93a605373aa64f
Hung Le <hung.le.ext@ericsson.com>
2026-09-07 22:21:51 +0700

    ci(e2e): add test failure summary and enrich playwright reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,17 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? [['list'], ['html', { open: "never" }]] : "list",
+  reporter: process.env.CI
+    ? [
+        ["list"],
+        ["github"],
+        ["html", { open: "never" }],
+        ["json", { outputFile: "test-results/results.json" }],
+      ]
+    : [
+        ["list"],
+        ["json", { outputFile: "test-results/results.json" }],
+      ],
   use: {
     baseURL: "http://localhost:3000",
     trace: "on-first-retry",
